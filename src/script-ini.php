4260d0a89288a0e3817389bd8c192efb59e75dc9
Luiz Carlos <argentinaluiz@gmail.com>
2015-02-20 10:50:38 -0200

    Add message when terminate execution script

--- src/script-ini.php
+++ src/script-ini.php
@@ -7,4 +7,4 @@ chmod('resources/log', 0777);
 chmod('web/assets', 0777);
 chmod('console', 0500);
 
-exec("php \"$rootDir/console\" assetic:dump");
+echo exec("php \"$rootDir/console\" assetic:dump");
